6f70ca45de372b4633265d2693255f7b0f0f33a1
Larry Lu <pudding850806@gmail.com>
2018-07-03 15:42:50 +0800

    Setup PTT crawler development environment

---
+++ ptt_crawler/index.js
@@ -0,0 +1,11 @@
+/**
+ * HTTP Cloud Function.
+ * This function is exported by index.js, and is executed when
+ * you make an HTTP request to the deployed function's endpoint.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloGET = (req, res) => {
+  res.send('Hello World!')
+}
