471d3203a2abcf5ce8b70e9150173629257b55bb
Larry Lu <pudding850806@gmail.com>
2018-11-22 16:06:56 +0800

    Finish get beauties from api

--- mail_sender2/api/api.go
+++ mail_sender2/api/api.go
@@ -1,21 +1,39 @@
-	// // "main/mail"
-	// "encoding/json"
-	// "fmt"
-	// "io/ioutil"
-	// "net/http"
+package api
 
-	// "main/model"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
-	// url := "https://us-central1-daily-beauty-209105.cloudfunctions.net/getDailyBeauties"
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// body, _ := ioutil.ReadAll(resp.Body)
+	"main/model"
+)
 
-	// beauties := make([]model.Beauty, 3)
-	// err = json.Unmarshal(body, &beauties)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("%#v\n", beauties)
+const url = "https://us-central1-daily-beauty-209105.cloudfunctions.net/getDailyBeauties"
+
+// FetchBeauties send a request to get beauties from getDailyBeauties api
+func FetchBeauties() ([]model.Beauty, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	beauties := make([]model.Beauty, 3)
+	err = json.Unmarshal(body, &beauties)
+
+	if err != nil {
+		return nil, err
+	}
+
+	beauties[0].Rank = "一"
+	beauties[1].Rank = "二"
+	beauties[2].Rank = "三"
+
+	// [正妹] 大橋未久 -> 大橋未久
+	for i := range beauties {
+		beauties[i].Title = beauties[i].Title[9:]
+	}
+
+	return beauties, nil
+}
--- mail_sender2/model/beauty.go
+++ mail_sender2/model/beauty.go
@@ -2,6 +2,7 @@ package model
 
 // Beauty is a struct from getDailyBeauties api
 type Beauty struct {
+	Rank       string // "一", "二", "三"
 	NVote      int
 	Title      string
 	Href       string
