65f7073bb046fb445c3f79c72ba99ffd0423d8b9
Larry Lu <pudding850806@gmail.com>
2018-11-22 16:52:32 +0800

    Remove useless file

--- subscribe_api/index.js
+++
@@ -1,8 +0,0 @@
-// ref: https://cloud.google.com/functions/docs/writing/http
-exports.subscribe = co.wrap(function*(req, res) {
-  res.send('subscribe success')
-})
-
-exports.unsubscribe = co.wrap(function*(req, res) {
-  res.send('unsubscribe success')
-})
