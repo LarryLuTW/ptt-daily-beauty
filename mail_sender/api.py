aa556d096220e1e85247eb4bb1e999dc1030e125
Larry Lu <pudding850806@gmail.com>
2018-11-22 16:15:32 +0800

    Remove old mail sender

--- mail_sender/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def getBeauties():
-    url = 'https://us-central1-daily-beauty-209105.cloudfunctions.net/getDailyBeauties'
-    r = requests.get(url)
-    beauties = r.json()
-    # Sort by popularity ascendantly
-    return list(reversed(beauties))
