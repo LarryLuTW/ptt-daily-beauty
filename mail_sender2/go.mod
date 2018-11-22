7a22c0e570964ac2f3505d63a5c6c5acdcc47bac
Larry Lu <pudding850806@gmail.com>
2018-11-22 16:07:03 +0800

    Finish send daily beauty

--- mail_sender2/go.mod
+++ mail_sender2/go.mod
@@ -1,1 +1,3 @@
 module main
+
+require github.com/vjeantet/jodaTime v0.0.0-20170816150230-be924ce213fb
--- mail_sender2/main.go
+++ mail_sender2/main.go
@@ -1,32 +1,68 @@
 package main
 
 import (
-	// "main/mail"
-
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
+
+	"github.com/vjeantet/jodaTime"
+
+	"main/api"
+	"main/mail"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+func init() {
+	time.LoadLocation("NST") // set timezone to taiwan
+}
+
+func sendDailyBeauty(subscribers []string, isTest bool) {
+	beauties, err := api.FetchBeauties()
+
+	if err != nil {
+		panic(err)
+	}
+
+	html := mail.GenerateHTML(beauties)
+
+	date := jodaTime.Format("YYYY-MM-dd", time.Now())
+	subject := fmt.Sprintf("表特日報-%s", date)
+
+	if isTest {
+		subject += " " + strconv.Itoa(rand.Int())
+	}
+
+	for _, to := range subscribers {
+		mail.Send(to, subject, html)
+	}
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	sendDailyBeauty([]string{"pudding850806@gmail.com"}, true)
+	log.Println("Test successfully")
+	w.Write([]byte("Test successfully"))
+}
+
+func publishHandler(w http.ResponseWriter, r *http.Request) {
+	// toMails := ['pudding850806@gmail.com', 'w5151381guy@gmail.com', 'vorkibiz@gmail.com']
+	sendDailyBeauty([]string{"pudding850806@gmail.com", "pudding850806+10@gmail.com"}, false)
+	log.Println("Publish successfully")
+	w.Write([]byte("Publish successfully"))
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/publish", publishHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Printf("port %s", port)
-	http.ListenAndServe(":"+port, nil)
-
-	// resp.Body
-
-	// to := "pudding850806@gmail.com"
-	// subject := "This is subject"
-	// html := "<h1> Heading 123 </h1> 123"
-	// mail.Send(to, subject, html)
+	log.Printf("listen on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	panic(err)
 }
