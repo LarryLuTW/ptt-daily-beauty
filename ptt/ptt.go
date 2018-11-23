3e74f1415376d018d489c2744816bf2a176a7e3d
Larry Lu <pudding850806@gmail.com>
2018-11-23 11:57:50 +0800

    Refactor ptt.go

--- ptt/ptt.go
+++ ptt/ptt.go
@@ -21,51 +21,57 @@ type post struct {
 	date  time.Time
 }
 
-// TODO: function too big
-func fetchYesterPosts() []post {
+// FIXME: sometimes PTT cache search result
+func fetchSearchResult(prefix string, page, recommend int) ([]post, error) {
 	baseURL := "https://www.ptt.cc/bbs/Beauty/search"
-	keyword := "["
+	url := fmt.Sprintf("%s?page=%d&q=[+recommend:%d", baseURL, page, recommend)
+	doc, err := goquery.NewDocument(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	posts := make([]post, 0, 20)
+	doc.Find(".r-ent").Each(func(i int, el *goquery.Selection) {
+		nVoteText := el.Find(".hl").Text()
+		nVote := parseNVote(nVoteText)
+
+		titleEl := el.Find(".title > a")
+		title := titleEl.Text()
+
+		if !strings.HasPrefix(title, prefix) {
+			return
+		}
+
+		hrefText, _ := titleEl.Attr("href")
+		href := "https://www.ptt.cc" + hrefText
+
+		currentYear := time.Now().Year()
+		dateText := fmt.Sprintf("%d/%s", currentYear, el.Find(".meta .date").Text())
+		date, _ := jodaTime.ParseInLocation("YYYY/MM/dd", dateText, "Asia/Taipei")
+
+		p := post{
+			title: title,
+			href:  href,
+			nVote: nVote,
+			date:  date,
+		}
+
+		posts = append(posts, p)
+	})
+
+	return posts, nil
+}
+
+// TODO: return error
+func fetchYesterdayPosts() []post {
+	prefix := "[正妹]"
 	posts := make([]post, 0, 20)
 
 	page := 1
-
 	for {
-
-		url := fmt.Sprintf("%s?page=%d&q=%s+recommend:1", baseURL, page, keyword)
-
-		doc, err := goquery.NewDocument(url)
-
-		if err != nil {
-			panic(err)
-		}
-
-		doc.Find(".r-ent").Each(func(i int, el *goquery.Selection) {
-			nVoteText := el.Find(".hl").Text()
-			nVote := parseNVote(nVoteText)
-
-			titleEl := el.Find(".title > a")
-			title := titleEl.Text()
-
-			if !strings.HasPrefix(title, "[正妹]") {
-				return
-			}
-
-			hrefText, _ := titleEl.Attr("href")
-			href := "https://www.ptt.cc" + hrefText
-
-			currentYear := time.Now().Year()
-			dateText := fmt.Sprintf("%d/%s", currentYear, el.Find(".meta .date").Text())
-			date, _ := jodaTime.ParseInLocation("YYYY/MM/dd", dateText, "Asia/Taipei")
-
-			p := post{
-				title: title,
-				href:  href,
-				nVote: nVote,
-				date:  date,
-			}
-
-			posts = append(posts, p)
-		})
+		ps, _ := fetchSearchResult(prefix, page, 1)
+		posts = append(posts, ps...)
 
 		oldestDate := posts[len(posts)-1].date
 		if isBeforeYesterday(oldestDate) {
@@ -128,7 +134,7 @@ func getChampions(posts []post) []model.Beauty {
 
 // FetchBeauties send a request to get beauties from getDailyBeauties api
 func FetchBeauties() ([]model.Beauty, error) {
-	posts := fetchYesterPosts()
+	posts := fetchYesterdayPosts()
 	beauties := getChampions(posts)
 
 	return beauties, nil
