68f5d194e9c00d58bb0f66c0971de4d11c554447
Larry Lu <pudding850806@gmail.com>
2018-11-27 14:31:12 +0800

    Enhance parseNVote

--- ptt/parser.go
+++ ptt/parser.go
@@ -7,11 +7,19 @@ import (
 
 // parseNVote parses vote text to int
 // "50" => 50, "爆" => 100
+// "" => 0
+// "X7" => -1
 // there is no need to handle nVote <= 0
 // because they are filterer out when searching
 func parseNVote(nVoteText string) int {
 	if nVoteText == "爆" {
 		return 100
+	}
+	if nVoteText == "" {
+		return 0
+	}
+	if strings.HasPrefix(nVoteText, "X") {
+		return -1
 	}
 	nVote, _ := strconv.Atoi(nVoteText)
 	return nVote
